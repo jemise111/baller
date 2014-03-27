9680d9d9cba9af11fda694a18f845d2497f9efe2
jemise111 <jesse.sessler@gmail.com>
2014-03-27 14:06:11 -0400

    initialize rails and create users mvc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Baller::Application.config.secret_key_base = '411aa83ca4249ca222f34041b4fc642e94e822a4e74fc12532ceb610fc6c219ebd6b6c0704df26f413f235c869e50beff32ef95a89a6f38398169baeb622b2f1'
