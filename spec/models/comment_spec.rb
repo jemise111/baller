dc7f25438ea8fdb44a4c13a7e2cd79393f816a6c
jemise111 <jesse.sessler@gmail.com>
2014-04-13 23:30:04 -0400

    add shoulda tests to comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe Comment do
+
+  it { should belong_to(:user) }
+  it { should belong_to(:court) }
+  it { should validate_presence_of(:body) }
+
+  # it do
+  #    should ensure_length_of(:body).
+  #     is_at_least(1).
+  #     is_at_most(200).
+  #     with_short_message(/contain text/).
+  #     with_long_message(/too long/)
+  # end
+
   subject(:comment) { Comment.create(body: 'Court has sweet rims, no cracks, lots of players.') }
   before(:each) do
     comment.update(approved: false)
